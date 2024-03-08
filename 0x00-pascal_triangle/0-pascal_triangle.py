e44dd5c8f6208cfd20dafccfded70d365e5236d7
Eztosin <tosinezekiel03@gmail.com>
2024-03-08 23:04:55 +0000

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pas_tri = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                prev_row = pas_tri[i - 1]
+                prev_val_l = prev_row[j - 1] if j - 1 < len(prev_row) else 0
+                prev_val_r = prev_row[j] if j < len(prev_row) else 0
+                row.append(prev_val_l + prev_val_r)
+        pas_tri.append(row)
+    return pas_tri
